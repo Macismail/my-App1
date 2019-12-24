bba4d5f9e0c67b45c5c63e0e225507f39c3bb568
macismail <samouel_77@hotmail.com>
2019-12-24 22:40:01 +0000

    f push

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Button, Form, FormGroup, Alert, Card, Container, Row, Col} from "react-bootstrap";
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      valu : "This is the state content",
+      count: 0
+    }
+    this.change = this.change.bind(this)
+  }
+
+  change (){
+    this.setState(prevstate => ({
+      count : prevstate.count+1
+    }))
+  }
+
+  render(){
+    return (
+      <div className="root-container">
+      <Container>
+        <Row>
+          <Col></Col>
+          <Col sm={6}>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={logo} />
+              <Card.Body>
+                <Card.Title>{this.state.valu}</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col></Col>
+        </Row>
+          <Alert variant="primary"> Primary <b>{this.state.count}</b> </Alert>
+          <Button variant="success" onClick={this.change}>Add 1</Button>
+          <Button variant="warning">Warning</Button>
+          <Button variant="danger">Danger</Button>
+          <Button variant="info">Info</Button>
+          <Button variant="link">Link</Button>
+          <Alert variant="success"> Success </Alert>
+
+
+        </Container>    
+      </div>
+    )
+  }
+}
+
+class SomeCode extends Component{
+  render(){
+    return(
+      <div>
+        <container>
+          <h1>Some Text to display</h1>
+        </container>
+      </div>
+    )
+  }
+}
+
+export default App;
