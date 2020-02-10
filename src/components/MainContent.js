4348d6eb2477bcd19b18c28a802156cd739b3c52
macismail <samouel_77@hotmail.com>
2020-02-10 17:13:23 +0000

    change heroku name and some modi

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -24,8 +24,7 @@ class MainContent extends Component {
       <div>
         <Container>
         <Row>
-          <Col sm={4}></Col>
-          <Col sm={6}>
+          <Col md={4}>
             <Card style={{ width: '18rem' }}>
               <Card.Img variant="top" src={logo} />
               <Card.Body>
@@ -38,7 +37,32 @@ class MainContent extends Component {
               </Card.Body>
             </Card>
           </Col>
-          <Col></Col>
+          <Col md={4}>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={logo} />
+              <Card.Body>
+                <Card.Title>{this.state.valu}</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4}>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={logo} />
+              <Card.Body>
+                <Card.Title>{this.state.valu}</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
         </Row>
           <Alert variant="primary"> Primary <b>{this.state.count}</b> </Alert>
           <Button variant="success" onClick={this.change}>Add 1</Button>
